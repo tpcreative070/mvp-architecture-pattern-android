4097f723a47b852bac8fbc54e2834366f64826a7
TPhong <tpalwayscreative@gmail.com>
2018-06-01 11:25:55 +0700

    add

---
+++ app/src/main/java/tpcreative/co/mvp_architecture_pattern_android/MainActivity.java
@@ -0,0 +1,14 @@
+package tpcreative.co.mvp_architecture_pattern_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
